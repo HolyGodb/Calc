d7740a474e95a5eb18d1997d6473839a72c63816
HolyGodb <43905817+HolyGodb@users.noreply.github.com>
2018-11-09 18:21:07 +0200

    message

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,17 @@
+public class Calculator {
+    public double Add(double a, double b) {
+        return a + b;
+    }
+
+    public double Substract(double a, double b) {
+        return a - b;
+    }
+
+    public double Divide(double a, double b) {
+        return a / b;
+    }
+
+    public double Multiply(double a, double b) {
+        return a * b;
+    }
+}
---
+++ src/test/java/TestCalc.java
@@ -0,0 +1,39 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestCalc {
+    Calculator calculator = new Calculator();
+
+    //Pass
+    @Test
+    public void CheckSum() {
+        Assert.assertEquals(calculator.Add(10, 15), 25.0);
+    }
+
+    @Test
+    public void CheckSubstract() {
+        Assert.assertNotNull(calculator.Substract(10, 15));
+    }
+
+    @Test
+    public void CheckDivide() {
+        Assert.assertTrue(calculator.Divide(4, 2) == 2.0);
+    }
+
+    @Test
+    public void CheckMultiply() {
+        Assert.assertEquals(calculator.Multiply(10, 15), 150.0);
+    }
+
+    //Fail
+    @Test
+    public void CheckFailAdd() {
+        Assert.assertFalse(calculator.Divide(4, 2) == 2.1);
+    }
+
+    @Test
+    public void CheckFailSubstract()
+    {
+        Assert.assertFalse(calculator.Substract(5,3) == 2.1);
+    }
+}
